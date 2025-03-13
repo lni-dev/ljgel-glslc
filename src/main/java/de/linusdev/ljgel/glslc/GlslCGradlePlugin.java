5722f999e7456ead72e46bfea8f5f178831893d8
LinusDev <linus@linusdev.de>
2025-03-13 15:35:21 +0100

    Initial commit

---
+++ src/main/java/de/linusdev/ljgel/glslc/GlslCGradlePlugin.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2024 Linus Andera
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.linusdev.ljgel.glslc;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.tasks.SourceSet;
+import org.gradle.api.tasks.SourceSetContainer;
+import org.gradle.api.tasks.TaskProvider;
+import org.jetbrains.annotations.NotNull;
+
+public class GlslCGradlePlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(@NotNull Project target) {
+        TaskProvider<GlslCTask> glslcTask = target.getTasks().register("glslc", GlslCTask.class);
+
+        target.getPlugins().withId("java", plugin -> {
+            target.afterEvaluate(proj -> {
+                SourceSetContainer sourceSets = target.getExtensions().getByType(SourceSetContainer.class);
+                SourceSet mainSourceSet = sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME);
+
+                // Add src
+                mainSourceSet.getResources().srcDir(glslcTask);
+            });
+        });
+    }
+}
